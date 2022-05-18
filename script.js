1c041b0b173ec5a854043180d3a26a1a8d1c477f
NickSchenck <yoyo4games@yahoo.com>
2022-05-18 16:40:57 -0500

    Basic idea(4 character values, 4 window confirms)

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Assignment code here
-
+var lowerValue = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperValue = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numericValue = ["1","2","3","4","5","6","7","8","9","0"]
+var specialValue = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  window.confirm("Would you like your password to contain lowercase characters?")
+  window.confirm("Would you like your password to contain uppercase characters?")
+  window.confirm("Would you like your password to contain numeric characters?")
+  window.confirm("Would you like your password to contain special characters?")
+  console.log(window.confirm)
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
