d23ca100b79a91eb5c470a23a282f7791a961788
NickSchenck <yoyo4games@yahoo.com>
2022-05-18 18:53:19 -0500

    arrays are random, input is required

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Assignment code here
-var lowerValue = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upperValue = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var numericValue = ["1","2","3","4","5","6","7","8","9","0"]
-var specialValue = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
+var lowerValues = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperValues = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numericValues = ["1","2","3","4","5","6","7","8","9","0"]
+var specialValues = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
 var password = ""
+var randomLower = lowerValues[~~(Math.random() * lowerValues.length)];
+var randomUpper = upperValues[~~(Math.random() * upperValues.length)];
+var randomNumeric = numericValues[~~(Math.random() * numericValues.length)];
+var randomSpecial = specialValues[~~(Math.random() * specialValues.length)];
+  console.log(randomLower);
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -11,24 +17,29 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   password = ""
   let lowerClicked = window.confirm("Would you like your password to contain lowercase characters?")
-  console.log(lowerClicked);
+  console.log("randomLower=" + randomLower);
   if(lowerClicked === true){
-    password += lowerValue[0];
+    //var range = new Range(0, 25);
+    password += randomLower;
   }
   let upperClicked = window.confirm("Would you like your password to contain uppercase characters?")
-  console.log(upperClicked);
+  console.log("randomUpper=" + randomUpper);
   if(upperClicked === true){
-    password += upperValue[0];
+    password += randomUpper;
   }
   let numberClicked = window.confirm("Would you like your password to contain numeric characters?")
-  console.log(numberClicked);
+  console.log("randomNumber=" + randomNumeric);
   if(numberClicked === true){
-    password += numericValue[0];
+    password += randomNumeric;
   }
   let specialClicked = window.confirm("Would you like your password to contain special characters?")
-  console.log(specialClicked);
+  console.log("randomSpecial=" + randomSpecial);
   if(specialClicked === true){
-    password += specialValue[0];
+    password += randomSpecial;
+  }
+  if(password === "" || password === null){
+    window.alert("You must select at least one character type!");
+    return writePassword();
   }
   //var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -39,3 +50,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
