b314b37b025f6acc060bda0c325561259a7e0c14
unknown <yoyo4games@Yahoo.com>
2024-05-17 15:46:37 -0500

    Rewrite mostly done, needs a bit more testing. Current glitch noted at bottom, otherwise app is very cleaned-up

--- script.js
+++ script.js
@@ -1,104 +1,89 @@
-//Generator currently doesn't prevent unwanted characters from generating in the password
+//values which can be allowed or denied ability to generate in generator
 const lowerValues = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const upperValues = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]  //All character arrays
 const numericValues = ["1","2","3","4","5","6","7","8","9","0"]
 const specialValues = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
-const defaultLength = Math.floor(Math.random() * 16) + 6; //randomized password length
 
+//instantiated values to act as containers. password will hold the randomized password, allValues will hold
+//the selected arrays above
+let password = "";
+let allValues = []; 
 
-let password = ""
-//potentially move these concat values into the if statements?
-let allValues = []; /*lowerValues.concat(upperValues).concat(numericValues).concat(specialValues);*/ //combines arrays to make selection more random
+//helper function, randomizes a number within a range. Within this app we randomize min = 6 and max = 16
+function randomNumber(min, max) {
+  let value = Math.floor(Math.random() * (max - min) + min);
 
-//these variables are generating a number within the [], for the array of characters they belong to
-//potential future issue; Math.random is upper-bound EXclusive, so it may never roll the final character in a given array as is
-//Also not sure these lower 4 variables will be necessary, with how I'm setting-up getValues
-let randomLower = lowerValues[~~(Math.random() * lowerValues.length)];
-let randomUpper = upperValues[~~(Math.random() * upperValues.length)];
-let randomNumeric = numericValues[~~(Math.random() * numericValues.length)]; //Math for a random value within arrays
-let randomSpecial = specialValues[~~(Math.random() * specialValues.length)];
+  return value;
+};
 
-
-
-// Get references to the #generate element
-// let generateBtn = document.querySelector("#generate");
-
+//getValues determines which value-arrays are added to the allValues container by evaluating if the user
+//selected "OK"(evals to true) or "Cancel"(evals to false), upon clicking the "Generate Password" button.
+//finally, getValues calls the randomizePassword function.
 function getValues(){
+
   let selectLower = window.confirm("Would you like your password to contain lowercase characters?");
   if(selectLower === true){
     allValues = allValues.concat(lowerValues);
-    console.log(allValues);
   };
 
   let selectUpper = window.confirm("Would you like your password to contain uppercase characters?");
   if(selectUpper === true){
     allValues = allValues.concat(upperValues);
-    console.log(allValues);
   };
 
   let selectNumeric = window.confirm("Would you like your password to contain numeric characters?");
   if(selectNumeric === true){
     allValues = allValues.concat(numericValues);
-    console.log(allValues);
   };
 
   let selectSpecial = window.confirm("Would you like your password to contain special characters?");
   if(selectSpecial === true){
     allValues = allValues.concat(specialValues);
-    console.log(allValues);
   };
-  
+
+  randomizePassword();
 };
-console.log(allValues);
-// Write password to the #password input
-// function writePassword() {
-//   let selectLower = window.confirm("Would you like your password to contain lowercase characters?")
-//   console.log("randomLower=" + randomLower);
-//   if(selectLower === true){
-    
-//     allValues = allValues.concat(lowerValues);
-//   }
-//   let upperClicked = window.confirm("Would you like your password to contains uppercase characters?")
-//   console.log("randomUpper=" + randomUpper);
-//   if(upperClicked === true){    
-                                                                                
-//     password += randomUpper;
-//   }
-//   let numberClicked = window.confirm("Would you like your password to contain numeric characters?")
-//   console.log("randomNumber=" + randomNumeric);
-//   if(numberClicked === true){
-    
-//     password += randomNumeric;
-//   }
-//   let specialClicked = window.confirm("Would you like your password to contain special characters?")
-//   console.log("randomSpecial=" + randomSpecial);
-//   if(specialClicked === true){
-    
-//     password += randomSpecial;
-//   }
-//   let difference = defaultLength - password.length;
-  
-  
-//   for (let i = 0; i < difference; i++) {
-//     let randomAll = allValues[~~(Math.random() * allValues.length)];
-//     console.log(" allValues.length=" +  allValues.length);
-//     console.log(allValues);
-//     console.log("randomAll=" + randomAll);
-//     password += randomAll;
-//   }
-//   if(password === "" || password === null){
-//     window.alert("You must select at least one character type!"); //Requires user select at least one prompt to select characters from
-//     return writePassword();
-//   }
-  
-//   let passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
-  
-  
-// }
+//randomizePassword generates the randomized password with a for loop, checking if the variable i is less-than
+//the random number generated by the randomNumber helper function. Within the for loop, it enters an if statement
+//which checks that the user has selected at least one type of character to populate their randomized password.
+//as long as randomAll does not equal undefined, it will add the character selected by randomAll to the password
+//variable within the else statement. Last, randomizePassword calls the writePassword function.
+function randomizePassword(){
 
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+  for (let i = 0; i < randomNumber(6, 16); i++) {
+        let randomAll = allValues[~~(Math.random() * allValues.length)];
 
-getValues();
+        if(randomAll === undefined){
+          window.alert(`Please reload and select at least one character-type to include in your password.`);
+          break;
+        }else{
+          password += randomAll;
+        };
+      };
+
+  writePassword();
+};
+
+//writePassword allows the value contained within the password variable to be written to the page by selecting
+//the element with and Id of #password, and manipulating its .value property
+function writePassword(){
+  let passwordText = document.querySelector("#password");
+
+  if(passwordText.value){
+    password = ``;
+    passwordText.value = ``;
+  }else{
+    passwordText.value = password;
+  };
+};
+
+//generateBtn variable for selecting an element with an Id of #generate, and an event listener using the
+//generateBtn variable to activate upon a "click" event.
+let generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", getValues);
+
+//Only current glitch; once the user has generated a password, they'll need to complete a cycle of the window
+//prompts to clear that password, then enter into another cycle to generate a new password. Would like it to
+//either auto-clear the password on button-click, or clear once the user has completed their second go-through
+//of window prompts.
